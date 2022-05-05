61e938045df730ca41c13e43bcceed6271488997
JoaoVitorLima242 <jlima0500@gmail.com>
2022-05-05 16:01:35 -0300

    add comentary

--- web/app/routes/routes.js
+++ web/app/routes/routes.js
@@ -1,9 +1,9 @@
 module.exports = (application) => {
-	application.get('/', (req, res) => {
-		application.app.controllers.index.index(application, req, res);
-	});
+	/* application.get('/', (req, res) => {
+		application.app.controllers.index.home(application, req, res);
+	}); */
 
-	application.get('/home', (req, res) => {
+	application.get('/', (req, res) => {
 		application.app.controllers.home.home(application, req, res);
 	});
 
